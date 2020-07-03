9a723c986bcba609a3b671e63d5024f999be54cd
josephnormandev <jnormantransactions@gmail.com>
2020-07-03 18:54:27 -0500

    init commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Page from './page';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Page />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/page.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class Page extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+	}
+
+	render()
+	{
+		return (
+			<div>
+				Test
+			</div>
+		);
+	}
+}
