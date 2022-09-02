9ba016b1e7bd5b248c0dad4c987f5f00d003e9e0
josephnormandev <jnormantransactions@gmail.com>
2022-09-02 18:35:00 -0400

    adds an env.PORT variable to control port externally

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"os"
 	"log"
 	"net/http"
   	"github.com/gorilla/mux"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
 	r := mux.NewRouter()
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./frontend/build")))
 
-	err := http.ListenAndServe(":80", r)
+	err := http.ListenAndServe(":" + port, r)
 	if err != nil {
 		log.Fatal(err)
 	}
