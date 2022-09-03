e8a3121f0ed1fc11cd925d199d314b658d98fff2
josephnormandev <jnormantransactions@gmail.com>
2022-09-03 16:14:26 -0400

    debug messages

--- main.go
+++ main.go
@@ -11,11 +11,18 @@ func main() {
 	port := os.Getenv("PORT")
 
 	r := mux.NewRouter()
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./frontend/build")))
+	r.PathPrefix("/").HandlerFunc(handleFrontend)
 
 	log.Printf("Running Server on port: %s\n", port)
 	err := http.ListenAndServe(":" + port, r)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func handleFrontend(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.RequestURI()
+	log.Printf("Received Request for jellothompson.com%s\n", url)
+	server := http.FileServer(http.Dir("./frontend/build"))
+	server.ServeHTTP(w, r)
 }
